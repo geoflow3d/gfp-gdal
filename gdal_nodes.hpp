36f5a9a5e43f6abd748b4cb6648d5fea50b4d800
Ylannl <r.y.peters@tudelft.nl>
2019-12-16 17:25:52 +0100

    harmonise formatting style

--- gdal_nodes.hpp
+++ gdal_nodes.hpp
@@ -2,87 +2,112 @@
 
 #include <ogrsf_frmts.h>
 
-namespace geoflow::nodes::gdal {
+namespace geoflow::nodes::gdal
+{
 
-  class OGRLoaderNode:public Node {
-    int layer_count = 0;
-    int layer_id = 0;
-    float base_elevation = 0;
+class OGRLoaderNode : public Node
+{
+  int layer_count = 0;
+  int layer_id = 0;
+  float base_elevation = 0;
 
-    std::string filepath = "";
+  std::string filepath = "";
 
-    std::string geometry_type_name;
-    OGRwkbGeometryType geometry_type;
-    
-    void push_attributes(OGRFeature& poFeature);
+  std::string geometry_type_name;
+  OGRwkbGeometryType geometry_type;
 
-    public:
-    using Node::Node;
-    void init() {
-      add_vector_output("line_strings", typeid(LineString));
-      add_vector_output("linear_rings", typeid(LinearRing));
+  void push_attributes(OGRFeature &poFeature);
 
-      add_poly_output("attributes", {typeid(vec1b), typeid(vec1i), typeid(vec1f), typeid(vec1s)});
+public:
+  using Node::Node;
+  void init()
+  {
+    add_vector_output("line_strings", typeid(LineString));
+    add_vector_output("linear_rings", typeid(LinearRing));
 
-      add_param("filepath", ParamPath(filepath, "File path"));
-      add_param("base_elevation", ParamFloat(base_elevation, "Base elevation"));
-      // add_param("layer_id", ParamInt(layer_id, "Layer ID"));
+    add_poly_output("attributes", {typeid(vec1b), typeid(vec1i), typeid(vec1f), typeid(vec1s)});
 
-      GDALAllRegister();
-    }
-    void process();
-  };
+    add_param("filepath", ParamPath(filepath, "File path"));
+    add_param("base_elevation", ParamFloat(base_elevation, "Base elevation"));
+    // add_param("layer_id", ParamInt(layer_id, "Layer ID"));
 
-  class OGRWriterNode:public Node {
-    int epsg = 7415;
-    std::string filepath = "out";
-    
-    public:
-    using Node::Node;
-    void init() {
-      add_vector_input("geometries", { typeid(LineStringCollection), typeid(LinearRingCollection) });
+    GDALAllRegister();
+  }
+  void process();
+};
 
-      add_poly_input("attributes", {typeid(vec1b), typeid(vec1i), typeid(vec1f), typeid(vec1s)}, true);
+class OGRWriterNode : public Node
+{
+  int epsg = 7415;
+  std::string filepath = "out";
 
-      add_param("filepath", ParamPath(filepath, "File path"));
-    }
-    void process();
-  };
+public:
+  using Node::Node;
+  void init()
+  {
+    add_vector_input("geometries", {typeid(LineStringCollection), typeid(LinearRingCollection)});
+    add_poly_input("attributes", {typeid(vec1b), typeid(vec1i), typeid(vec1f), typeid(vec1s)}, true);
 
-  class CSVLoaderNode:public Node {
-    std::string filepath = "out";
-    int thin_nth = 5;
-  public:
-    using Node::Node;
-    void init() {
-      add_output("points", typeid(PointCollection));
+    add_param("filepath", ParamPath(filepath, "File path"));
+  }
+  void process();
+};
 
-      add_param("filepath", ParamPath(filepath, "File path"));
-      add_param("thin_nth", ParamBoundedInt(thin_nth, 0, 100, "Thin factor"));
-    }
-    void process();
-  };
+class CSVLoaderNode : public Node
+{
+  std::string filepath = "out";
+  int thin_nth = 5;
 
-  class CSVWriterNode:public Node {
-    std::string filepath = "out";
-  public:
-    using Node::Node;
-    void init() {
-      add_input("points", typeid(PointCollection));
-      add_input("distances", typeid(vec1f));
+public:
+  using Node::Node;
+  void init()
+  {
+    add_output("points", typeid(PointCollection));
 
-      add_param("filepath", ParamPath(filepath, "File path"));
-    }
-    void process();
-  };
+    add_param("filepath", ParamPath(filepath, "File path"));
+    add_param("thin_nth", ParamBoundedInt(thin_nth, 0, 100, "Thin factor"));
+  }
+  void process();
+};
 
-  class GEOSMergeLinesNode:public Node {
-  public:
-    using Node::Node;
-    void init() {
-      add_input("lines", typeid(LineStringCollection));
-      add_output("lines", typeid(LineStringCollection));
-    }
-    void process();
-  };
-}
+class CSVWriterNode : public Node
+{
+  std::string filepath = "out";
+
+public:
+  using Node::Node;
+  void init()
+  {
+    add_input("points", typeid(PointCollection));
+    add_input("distances", typeid(vec1f));
+
+    add_param("filepath", ParamPath(filepath, "File path"));
+  }
+  void process();
+};
+
+class GEOSMergeLinesNode : public Node
+{
+public:
+  using Node::Node;
+  void init()
+  {
+    add_input("lines", typeid(LineStringCollection));
+    add_output("lines", typeid(LineStringCollection));
+  }
+  void process();
+};
+
+class PolygonUnionNode : public Node
+{
+public:
+  using Node::Node;
+  void init()
+  {
+    add_vector_input("polygons", typeid(LinearRing));
+    add_vector_output("polygons", typeid(LinearRing));
+    add_vector_output("holes", typeid(LinearRing));
+  }
+  void process();
+};
+} // namespace geoflow::nodes::gdal
