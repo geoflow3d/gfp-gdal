9bd7b821a57052f6f78db903d6d99c19735e6bf5
Ylannl <r.y.peters@tudelft.nl>
2020-01-08 17:07:28 +0100

    update to new plugin architecture

--- register.hpp
+++ register.hpp
@@ -10,13 +10,3 @@ void register_nodes(geoflow::NodeRegister &node_register)
   node_register.register_node<CSVWriterNode>("CSVWriter");
   node_register.register_node<GEOSMergeLinesNode>("GEOSMergeLines");
 }
-
-namespace geoflow::nodes::gdal
-{
-NodeRegisterHandle create_register()
-{
-  auto R = NodeRegister::create(GF_PLUGIN_NAME);
-  register_nodes(*R);
-  return R;
-}
-} // namespace geoflow::nodes::gdal
